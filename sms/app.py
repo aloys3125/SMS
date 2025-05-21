0bc7dee3597d1feeda9c353bdbff00cc9cf5d9ea
aloys3125 <aloysalfred31@gmail.com>
2025-05-21 11:25:41 +0530

    modified app.py

--- sms/app.py
+++ sms/app.py
@@ -16,4 +16,5 @@ def home():
     return render_template("index.html", summary=summary, transcript=transcript)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080)
+
